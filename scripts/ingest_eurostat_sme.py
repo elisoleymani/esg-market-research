6aba9d9197ba453a99afb25bbfa3ce3c530e3b5d
Elham (Eli) Soleymani <77296471+elisoleymani@users.noreply.github.com>
2026-07-16 18:24:02 +0200

    ingest_eurostat_sme python script: adding the db connections

--- scripts/ingest_eurostat_sme.py
+++ scripts/ingest_eurostat_sme.py
@@ -1,6 +1,16 @@
 #ingest the dataset into sql db 
 import csv
 import sqlite3
+import pandas as pd
+
+#load the data
+df = pd.read_csv('../data/raw/sbs_sc_sca_r2$defaultview_linear_2_0.csv') 
+
+print(df.shape)
+print(df.columns.tolist())
+print(df.head(10))
+print(df.dtypes)
+
  # Connecting to the database
 con = sqlite3.connect('sme_research.db')
 msg = "connection check!"
